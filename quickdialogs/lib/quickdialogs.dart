f0775c7395f470ca36e160ac4b24145dd7ed6935
Tom <tom@myoxygen.co.uk>
2019-09-20 10:28:31 +0100

    Added code for handling dialogs. Also added tests.

--- quickdialogs/lib/quickdialogs.dart
+++ quickdialogs/lib/quickdialogs.dart
@@ -1,7 +1,186 @@
 library quickdialogs;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class QuickDialogs {
+  // Prevent multiple instances of QuickDialogs class being created.
+  QuickDialogs._();
+
+  /// Creates a dialog with the option for a destructive action.
+  /// [context] is required to build the dialog
+  /// [title] and [message] make up the dialog content
+  /// [destructiveActionName] will be an *uppercased* option on the dialog given in a red font.
+  /// [destructiveActionCallback] is the callback from the above action.
+  /// As well as the destructive action, there will be a "Cancel" option given
+  static void destructive(
+    BuildContext context, {
+    @required String title,
+    @required String message,
+    @required String constructiveActionName,
+    @required String destructiveActionName,
+    Function destructiveActionCallback,
+  }) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return _PlatformAlertDialog.stringContent(
+            title: title,
+            content: message,
+            actions: <Widget>[
+              FlatButton(
+                child: Text(
+                  constructiveActionName.toUpperCase(),
+                  style: const TextStyle(color: Colors.blue),
+                ),
+                onPressed: () {
+                  // Close the dialog.
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Text(destructiveActionName?.toUpperCase() ?? "",
+                    style: const TextStyle(color: Colors.red)),
+                onPressed: () {
+                  // Close the dialog.
+                  Navigator.of(context).pop();
+
+                  destructiveActionCallback?.call();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  /// Display a standard information dialog.
+  static void infoDialog(
+    BuildContext context,
+    String title,
+    String message, {
+    String okButton = "OK",
+    Function onOkClicked,
+  }) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return _PlatformAlertDialog.stringContent(
+            title: title,
+            content: message,
+            actions: <Widget>[
+              _PlatformDialogButton.stringContent(
+                text: okButton ?? "OK",
+                onPressed: () {
+                  // Dialog is part of the Navigator.
+                  // This will just close the *dialog*.
+                  Navigator.pop(context);
+
+                  onOkClicked?.call();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  /// Display a confirmation dialog, and set the required positive and negative
+  /// buttons texts.
+  static Future<bool> confirmationDialogAsync({
+    BuildContext context,
+    String title,
+    String message,
+    String positiveButtonText,
+    String negativeButtonText,
+  }) async {
+    final bool result = await showDialog<bool>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return _PlatformAlertDialog.stringContent(
+            title: title,
+            content: message,
+            actions: <Widget>[
+              _PlatformDialogButton.stringContent(
+                text: negativeButtonText,
+                onPressed: () {
+                  // Dialog is pat of the Navigator.
+                  // This will just close the *dialog*.
+                  Navigator.pop(context, false);
+                },
+              ),
+              _PlatformDialogButton.stringContent(
+                text: positiveButtonText,
+                onPressed: () {
+                  // Return true for confirmation
+                  Navigator.pop(context, true);
+                },
+              ),
+            ],
+          );
+        });
+
+    return result;
+  }
+}
+
+class _PlatformAlertDialog extends StatelessWidget {
+  final String title;
+
+  //String content;
+  final Widget contentWidget;
+  final List<Widget> actions;
+
+  _PlatformAlertDialog.stringContent({String title, String content, List<Widget> actions})
+      : this.widgetContent(title: title, actions: actions, contentWidget: Text(content));
+
+  const _PlatformAlertDialog.widgetContent({this.title, this.contentWidget, this.actions});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget alert;
+    if (Theme.of(context).platform == TargetPlatform.iOS) {
+      // Don't show the title for iOS loading dialogs. This is not HIG compliant.
+      alert = CupertinoAlertDialog(
+        title: title.isEmpty ? null : Text(title),
+        content: contentWidget,
+        actions: actions == null ? [] : actions,
+      );
+    } else {
+      alert = AlertDialog(
+        title: Text(title),
+        content: contentWidget,
+        actions: actions == null ? [] : actions,
+      );
+    }
+
+    /// Prevent dialogs with huge font sizes.
+    return alert;
+  }
+}
+
+class _PlatformDialogButton extends StatelessWidget {
+  final Widget textWidget;
+  final Function onPressed;
+
+  _PlatformDialogButton.stringContent({@required String text, @required Function onPressed})
+      : this.widgetContent(textWidget: Text(text), onPressed: onPressed);
+
+  const _PlatformDialogButton.widgetContent({@required this.textWidget, @required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    if (Theme.of(context).platform == TargetPlatform.iOS) {
+      return CupertinoDialogAction(
+        child: textWidget,
+        onPressed: onPressed,
+      );
+    } else {
+      return FlatButton(
+        child: textWidget,
+        onPressed: onPressed,
+      );
+    }
+  }
 }
--- quickdialogs/test/quickdialogs_test.dart
+++ quickdialogs/test/quickdialogs_test.dart
@@ -1,13 +1,92 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:quickdialogs/quickdialogs.dart';
 
+MaterialApp _appWithDialog(Function(BuildContext) createDialog) {
+  return MaterialApp(
+    home: Material(
+      child: Builder(
+        builder: (BuildContext context) {
+          return Center(
+            child: RaisedButton(
+              child: const Text("Open Dialog"),
+              onPressed: () => createDialog(context),
+            ),
+          );
+        },
+      ),
+    ),
+  );
+}
+
+Future<void> _createAppOpenDialog(WidgetTester tester, Function(BuildContext) createDialog) async {
+  await tester.pumpWidget(_appWithDialog(createDialog));
+
+  await tester.tap(find.text("Open Dialog"));
+  await tester.pumpAndSettle();
+}
+
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  testWidgets("Info dialog opens and closes", (WidgetTester tester) async {
+    final createDialog = (context) => QuickDialogs.infoDialog(context, "MyTitle", "Some message.");
+    await _createAppOpenDialog(tester, createDialog);
+
+    final okTextFinder = find.text("OK");
+    expect(okTextFinder, findsOneWidget, reason: "The dialog should have opened.");
+
+    await tester.tap(okTextFinder);
+    await tester.pumpAndSettle();
+
+    expect(okTextFinder, findsNothing, reason: "The dialog should have closed.");
+  });
+
+  testWidgets("Destructive dialog opens and closes", (WidgetTester tester) async {
+    bool didDestroy = false;
+    final createDialog = (context) => QuickDialogs.destructive(
+          context,
+          title: "MyTitle",
+          message: "Delete?",
+          constructiveActionName: "Deny",
+          destructiveActionName: "Confirm",
+          destructiveActionCallback: () => didDestroy = true,
+        );
+    await _createAppOpenDialog(tester, createDialog);
+
+    final confirmTextFinder = find.text("Confirm".toUpperCase());
+
+    expect(confirmTextFinder, findsOneWidget, reason: "The dialog should have opened.");
+    expect(didDestroy, false, reason: "The destructive button should not have been tapped yet.");
+
+    await tester.tap(confirmTextFinder);
+    await tester.pumpAndSettle();
+
+    expect(confirmTextFinder, findsNothing, reason: "The dialog should have closed.");
+    expect(didDestroy, true, reason: "The destructive button should have been tapped.");
+  });
+
+  testWidgets("Confirmation dialog opens and closes", (WidgetTester tester) async {
+    bool didContinue = false;
+    final createDialog = (context) async {
+      didContinue = await QuickDialogs.confirmationDialogAsync(
+        context: context,
+        title: "MyTitle",
+        message: "Continue?",
+        positiveButtonText: "Yes",
+        negativeButtonText: "No",
+      );
+    };
+    await _createAppOpenDialog(tester, createDialog);
+
+    final confirmTextFinder = find.text("Yes");
+
+    expect(confirmTextFinder, findsOneWidget, reason: "The dialog should have opened.");
+    expect(didContinue, false, reason: "The positive button should not have been tapped yet.");
+
+    await tester.tap(confirmTextFinder);
+    await tester.pumpAndSettle();
+
+    expect(confirmTextFinder, findsNothing, reason: "The dialog should have closed.");
+    expect(didContinue, true, reason: "The positive button should have been tapped.");
   });
 }
