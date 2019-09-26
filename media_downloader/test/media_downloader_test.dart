222d0e9eb9a9bdd3c0dd3900fc8f22d70e05657f
Tom <tom@myoxygen.co.uk>
2019-09-26 12:02:44 +0100

    [MediaDownloader] Added test code.

--- media_downloader/test/media_downloader_test.dart
+++ media_downloader/test/media_downloader_test.dart
@@ -1,13 +1,77 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:media_downloader/media_downloader.dart';
 
+import 'package:mockito/mockito.dart';
+
+const _fileName = "file-loader-123";
+const _diskId = 222;
+const _url = "http://i.imgur.com/7SByB.jpg";
+
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  _TestFileSystem fileSystem;
+  _TestFileDownloader fileDownloader;
+  _FileLoader fileLoader;
+
+  setUp(() {
+    fileSystem = _TestFileSystem();
+    fileDownloader = _TestFileDownloader(fileSystem);
+    fileLoader = _FileLoader(fileDownloader: fileDownloader);
   });
+
+  test("FileLoader returns system file when present", () async {
+    when(fileSystem.getExistingFileFor(_fileName))
+        .thenAnswer((_) async => _TestFile(_diskId, _url));
+
+    final loaderResult = await fileLoader.getFile(_TestFile(123, _url));
+    expect(loaderResult.isPresent, true);
+
+    final file = loaderResult.value as _TestFile;
+    expect(file.id, equals(_diskId));
+  });
+
+  test("FileLoader returns empty optional when file not preent", () async {
+    when(fileSystem.getExistingFileFor(_fileName)).thenAnswer((_) async => null);
+
+    final loaderResult = await fileLoader.getFile(_TestFile(123, _url));
+    expect(loaderResult.isPresent, false);
+  });
+}
+
+class _TestFileDownloader extends Mock implements FileDownloader {
+  final FileSystem fileSystem;
+
+  _TestFileDownloader(this.fileSystem);
+}
+
+class _TestFileSystem extends Mock implements FileSystem {}
+
+class _TestFile extends Mock implements File {
+  final int id;
+  final String url;
+
+  _TestFile(this.id, this.url);
+}
+
+class _FileLoader extends BaseFileLoader<_TestFile> {
+  /// Want this to be sufficiently unique.
+  static const _fileNamePrefix = "file-loader";
+
+  _FileLoader({
+    @required FileDownloader fileDownloader,
+  }) : super(fileDownloader);
+
+  @override
+  ContentFileData dataForContent(_TestFile file) =>
+      ContentFileData(fileName: _makeFileName(file), dataUrl: file.url);
+
+  String _makeFileName(_TestFile file) {
+    return "$_fileNamePrefix-${file.id}";
+  }
+
+  Future deleteAllFiles() {
+    return deleteWhere((fileName) => fileName.startsWith(_fileNamePrefix));
+  }
 }
