a366007460713c0b3198dfe87b4c144cbcbb8840
JamesF <james.foot@myoxygen.co.uk>
2019-11-07 16:34:18 +0000

    [Unfocus Handler] Added the library (source SAM2)

--- unfocus_handler/lib/unfocus_handler.dart
+++ unfocus_handler/lib/unfocus_handler.dart
@@ -1,7 +1,24 @@
 library unfocus_handler;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class UnfocusHandler extends StatelessWidget {
+  final Widget child;
+
+  /// Wrap this around a view to automatically clear focus when the user
+  /// taps outside of the text field.
+  const UnfocusHandler({@required this.child}) : assert(child != null);
+
+  /// Clears any focus on the page.
+  static void clearFocus(BuildContext context) {
+    FocusScope.of(context).requestFocus(FocusNode());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: child,
+      onTap: () => clearFocus(context),
+    );
+  }
 }
--- unfocus_handler/test/unfocus_handler_test.dart
+++ unfocus_handler/test/unfocus_handler_test.dart
@@ -1,13 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:unfocus_handler/unfocus_handler.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  test("UnfocusHandler test", () {
+    expect(1 + 1, equals(2), reason: "Failed sanity check");
   });
 }
