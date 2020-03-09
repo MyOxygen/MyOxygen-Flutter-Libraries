6f2668cae2664bfb21d2e1fa6900515d23dc35e2
Tom <tom@myoxygen.co.uk>
2020-03-09 17:09:02 +0000

    [NavigationService] Added a navigation test

--- navigation_service/test/navigation_service_test.dart
+++ navigation_service/test/navigation_service_test.dart
@@ -1,13 +1,43 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:navigation_service/navigation_service.dart';
 
+import 'widgets/first_widget/first_widget.dart';
+import 'widgets/second_widget/second_widget_configuration.dart';
+
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  testWidgets("Can navigator navigate to and from a stateful page", (tester) async {
+    // Build the app layout
+    await tester.pumpWidget(MaterialApp(
+      navigatorKey: NavigationService.navigatorKey,
+      onGenerateRoute: (settings) => NavigationService.getRoute(settings),
+      home: FirstWidget(FirstWidgetConfiguration(
+        "/first",
+        () {
+          NavigationService.navigateTo(SecondWidgetConfiguration(
+            "/second",
+            () {
+              NavigationService.pop();
+            },
+          ));
+        },
+      )),
+    ));
+
+    expect(find.text('X'), findsOneWidget);
+    expect(find.text('Y', skipOffstage: false), findsNothing);
+
+    // Simulate pushing
+    await tester.tap(find.text('X'));
+    await tester.pumpAndSettle();
+    expect(find.text('X'), findsNothing);
+    expect(find.text('Y'), findsOneWidget);
+
+    // Simulate popping
+    await tester.tap(find.text('Y'));
+    await tester.pumpAndSettle();
+    expect(find.text('X'), findsOneWidget);
+    expect(find.text('Y'), findsNothing);
   });
 }
