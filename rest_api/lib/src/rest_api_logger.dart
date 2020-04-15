5f803c3836551c676576a771e4ce7fb6da9274ef
JamesF <james.foot@myoxygen.co.uk>
2020-04-15 17:11:22 +0100

    [RestApi] Enabled optional logging in production

--- rest_api/lib/src/rest_api_logger.dart
+++ rest_api/lib/src/rest_api_logger.dart
@@ -1,12 +1,19 @@
+import 'package:fimber/fimber.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 
 import '../rest_api.dart';
 
 class RestApiLogger {
+  final bool runInRelease;
+
+  bool get _isLogging => this.runInRelease || kDebugMode;
+
   /// Log the networking stuff. Keeps the logging code out of the
   /// main networking code.
-  const RestApiLogger();
+  const RestApiLogger({
+    this.runInRelease = false,
+  }) : assert(runInRelease != null);
 
   void logRequest(
     RestRequestType requestType,
@@ -14,68 +21,53 @@ class RestApiLogger {
     JsonObject jsonBody,
     Map<String, String> headers,
   }) {
-    // Do it in an assert so it only runs in debug mode,
-    assert(() {
-      _logRequest(requestType, url, jsonBody: jsonBody, headers: headers);
-      return true;
-    }());
-  }
-
-  void logException(dynamic exception) {
-    // do it in an assert so it only runs in debug mode,
-    assert(() {
-      _logException(exception);
-      return true;
-    }());
-  }
-
-  void logResponse(Response response) {
-    // do it in an assert so it only runs in debug mode,
-    assert(() {
-      _logResponse(response);
-      return true;
-    }());
-  }
-
-  /// Prints a request.
-  void _logRequest(
-    RestRequestType requestType,
-    String url, {
-    JsonObject jsonBody,
-    Map<String, String> headers,
-  }) {
-    debugPrint("---> ${_requestTypeName(requestType)} ${url ?? "NULL"}");
-    if (headers != null) {
-      headers.forEach((k, v) => debugPrint("---> $k : $v"));
-    }
-    if (jsonBody != null) {
-      final String jsonString = jsonBody.jsonString;
-      debugPrint("---> Body: $jsonString");
-    }
-  }
-
-  /// Prints an Exception.
-  void _logException(dynamic exception) {
-    debugPrint("<--- EXCEPTION");
-    if (exception != null) {
-      debugPrint("<--- ${exception.toString()}");
-    }
-  }
-
-  /// Prints a response.
-  void _logResponse(Response response) {
-    if (response == null) {
-      debugPrint("<--- Null response");
+    if (!_isLogging) {
       return;
     }
 
-    debugPrint("<--- ${response.statusCode} ${response.request.url}");
+    _log("---> ${_requestTypeName(requestType)} ${url ?? "NULL"}");
+    if (headers != null) {
+      headers.forEach((k, v) => _log("---> $k : $v"));
+    }
+
+    if (jsonBody != null) {
+      final String jsonString = jsonBody.jsonString;
+      _log("---> Body: $jsonString");
+    }
+  }
+
+  void logException(dynamic exception) {
+    if (!_isLogging) {
+      return;
+    }
+
+    _log("<--- EXCEPTION");
+    if (exception != null) {
+      _log("<--- ${exception.toString()}");
+    }
+  }
+
+  void logResponse(Response response) {
+    if (!_isLogging) {
+      return;
+    }
+
+    if (response == null) {
+      _log("<--- Null response");
+      return;
+    }
+
+    _log("<--- ${response.statusCode} ${response.request.url}");
     if (response.headers != null) {
-      response.headers.forEach((k, v) => debugPrint("<--- $k : $v"));
+      response.headers.forEach((k, v) => _log("<--- $k : $v"));
     }
     if (response.body != null) {
-      debugPrint("<--- Body: ${response.body}");
+      _log("<--- Body: ${response.body}");
     }
+  }
+
+  void _log(String text) {
+    Fimber.i(text ?? "");
   }
 
   // Just for pretty printing.
