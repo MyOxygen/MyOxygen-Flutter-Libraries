41441b0f34ad7c0ba92fd693df4a1248db1edcfe
tom-MO <42773461+tom-MO@users.noreply.github.com>
2020-12-21 15:13:27 +0000

    [UnfocusHandler] Added option to explicitly implement semantics

--- unfocus_handler/lib/unfocus_handler.dart
+++ unfocus_handler/lib/unfocus_handler.dart
@@ -4,10 +4,17 @@ import 'package:flutter/material.dart';
 
 class UnfocusHandler extends StatelessWidget {
   final Widget child;
+  final bool excludeFromSemantics;
 
   /// Wrap this around a view to automatically clear focus when the user
   /// taps outside of the text field.
-  const UnfocusHandler({@required this.child}) : assert(child != null);
+  /// To allow screen-readers to express "double tap to active" (or similar),
+  /// set `excludeFromSemantics` to `false`.
+  const UnfocusHandler({
+    @required this.child,
+    this.excludeFromSemantics = true,
+  })  : assert(child != null),
+        assert(excludeFromSemantics != null);
 
   /// Clears any focus on the page.
   static void clearFocus(BuildContext context) {
@@ -18,6 +25,7 @@ class UnfocusHandler extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       child: child,
+      excludeFromSemantics: excludeFromSemantics,
       onTap: () => clearFocus(context),
     );
   }
