778f707454f730619a8a2efacac08049e35c6538
tom-MO <42773461+tom-MO@users.noreply.github.com>
2021-02-01 17:48:42 +0000

    [ActionLog] Added delete button to the logs list.

--- action_log/lib/src/logs_list_viewer.dart
+++ action_log/lib/src/logs_list_viewer.dart
@@ -21,68 +21,81 @@ class LogsListViewer extends StatefulWidget {
 class _LogsListViewerState extends State<LogsListViewer> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
+  List<FileSystemEntity> listOfFiles;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        title: Text("Logs List"),
-        centerTitle: true,
-      ),
-      body: FutureBuilder<List<FileSystemEntity>>(
-        future: ActionLogHelper.getListOfLogs(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return ErrorDisplay(snapshot.error);
-          } else if (snapshot.hasData) {
-            final fileList = snapshot.data;
-            if (fileList.isEmpty) {
-              return Center(child: Text("No files to show."));
-            } else {
-              return ListView.builder(
-                itemCount: fileList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final fileName = basenameWithoutExtension(fileList[index].path);
-                  final formattedString = _formatFileName(fileName);
-                  if (formattedString == null) {
-                    return const SizedBox();
-                  }
-                  return ListTile(
-                    title: Text(formattedString),
-                    trailing: Icon(
-                      Icons.arrow_forward_ios,
-                      color: Colors.blue[300],
-                    ),
-                    onTap: () async {
-                      final fileDeleted = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => LogFileViewer(
-                            title: formattedString,
-                            fileSystemEntity: fileList[index],
-                          ),
-                        ),
-                      );
+    return FutureBuilder<List<FileSystemEntity>>(
+      future: ActionLogHelper.getListOfLogs(),
+      builder: (context, snapshot) {
+        Widget body;
 
-                      if (fileDeleted) {
-                        // We need to reload the page to stop displaying the
-                        // non-existing file.
-                        setState(() {});
-                        ActionLogHelper.displaySnackBar(
-                          _scaffoldKey.currentState,
-                          "Log file delete successfully.",
-                        );
-                      }
-                    },
-                  );
-                },
-              );
-            }
+        if (snapshot.hasError) {
+          body = ErrorDisplay(snapshot.error);
+        } else if (!snapshot.hasData) {
+          body = Center(child: CircularProgressIndicator());
+        } else {
+          listOfFiles = snapshot.data;
+          if (listOfFiles.isEmpty) {
+            body = Center(child: Text("No files to show."));
           } else {
-            return Center(child: CircularProgressIndicator());
+            body = ListView.builder(
+              itemCount: listOfFiles.length,
+              itemBuilder: (BuildContext context, int index) {
+                final fileName = basenameWithoutExtension(listOfFiles[index].path);
+                final formattedString = _formatFileName(fileName);
+                if (formattedString == null) {
+                  return const SizedBox();
+                }
+                return ListTile(
+                  title: Text(formattedString),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.blue[300],
+                  ),
+                  onTap: () async {
+                    final fileDeleted = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => LogFileViewer(
+                          title: formattedString,
+                          fileSystemEntity: listOfFiles[index],
+                        ),
+                      ),
+                    );
+
+                    if (fileDeleted) {
+                      // We need to reload the page to stop displaying the
+                      // non-existing file.
+                      setState(() {});
+                      ActionLogHelper.displaySnackBar(
+                        _scaffoldKey.currentState,
+                        "Log file delete successfully.",
+                      );
+                    }
+                  },
+                );
+              },
+            );
           }
-        },
-      ),
+        }
+
+        return Scaffold(
+            key: _scaffoldKey,
+            appBar: AppBar(
+              title: Text("Logs List"),
+              centerTitle: true,
+              actions: [
+                // Delete this log file
+                if (listOfFiles != null && listOfFiles.isNotEmpty)
+                  IconButton(
+                    icon: Icon(Icons.delete_forever),
+                    onPressed: _onDeletePressed,
+                  ),
+              ],
+            ),
+            body: body);
+      },
     );
   }
 
@@ -94,5 +107,30 @@ class _LogsListViewerState extends State<LogsListViewer> {
 
     final dateTime = DateTime.fromMicrosecondsSinceEpoch(timeStamp * 1000);
     return DateFormat("dd MMM yyyy - HH:mm:ss").format(dateTime);
+  }
+
+  Future<void> _onDeletePressed() async {
+    ActionLogHelper.displaySnackBar(
+      _scaffoldKey.currentState,
+      "Are you sure you wish to delete all log files?",
+      withAction: FlatButton(
+        child: Text("Delete all"),
+        textColor: Colors.red,
+        onPressed: () async {
+          String snackBarMessage = "Log files deleted.";
+          for (final file in listOfFiles) {
+            try {
+              await file.delete();
+            } catch (e) {
+              snackBarMessage = "Some log files could not be deleted. $e";
+            }
+          }
+
+          // Whether errors occur or not, we need to reload the page.
+          setState(() {});
+          ActionLogHelper.displaySnackBar(_scaffoldKey.currentState, snackBarMessage);
+        },
+      ),
+    );
   }
 }
