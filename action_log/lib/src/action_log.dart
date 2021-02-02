0295fd41bdbca9f24aa37b69c98c7c38eeb8fec9
tom-MO <42773461+tom-MO@users.noreply.github.com>
2021-02-02 11:36:25 +0000

    [ActionLog] Removed redundant functions

--- action_log/lib/src/action_log.dart
+++ action_log/lib/src/action_log.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
 import 'package:fimber_io/fimber_io.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
-import 'package:path/path.dart';
 
 import 'action_log_helper.dart';
 import 'logs_list_viewer.dart';
@@ -11,7 +8,6 @@ import 'logs_list_viewer.dart';
 class ActionLog {
   static String _filePath;
   static String _lastFileName;
-  static bool _isInitialised = false;
 
   /// Initialises the `ActionLog` class to be used throughout the code. Without
   /// this initialisation, no logging of any kind will occur. `isPublicRelease`
@@ -57,7 +53,6 @@ class ActionLog {
             "${CustomFormatTree.exceptionMsgToken}"));
     i("Logging initialised");
 
-    _isInitialised = true;
   }
 
   /// Writes an "information" level message to the logs. This is denoted by the
@@ -76,89 +71,6 @@ class ActionLog {
   /// letter "W" in the logs.
   static void w(String message, {dynamic ex}) {
     Fimber.w(message, ex: ex);
-  }
-
-  /// Reads the contents of the log files, and returns a `Map` containing the
-  /// contents of the log files separated by the log file names. An example:
-  /// ```
-  /// {
-  ///   "logFile1" : "Pressed button 1\nPressed button 2",
-  ///   "logFile2" : "Typed \"some text\"\nPressed submit button",
-  /// }
-  /// ```
-  static Future<Map<String, String>> readLogFiles() async {
-    final logFileMap = <String, String>{};
-
-    if (!_isInitialised) {
-      // ActionLog has not been initialised, so don't do anything.
-      logFileMap["NONE"] = "Uninitialised";
-      return logFileMap;
-    }
-
-    final fileDirectory = Directory(_filePath);
-
-    // Check that the folder containing the log files exists.
-    if (!await fileDirectory.exists()) {
-      logFileMap["NONE"] = "Log path does not exist";
-      return logFileMap;
-    }
-
-    // At any point during reading the files, the OS could do a funny and remove
-    // or change the files and/or directories. Wrap in a try/catch to prevent
-    // any funny business.
-    try {
-      final files = fileDirectory.listSync();
-      if (files == null || files.isEmpty) {
-        logFileMap["FILES"] = "No log files to read";
-        return logFileMap;
-      }
-
-      for (final fileEntity in files) {
-        await _updateLogFileMapWithFile(logFileMap, fileEntity);
-      }
-
-      // Log files might have been removed (if successful). We need to
-      // re-initialise, so that a new log file is assigned/created.
-      Fimber.clearAll();
-      // Set public release to false, as we shouldn't be at this point if it
-      // were a public release.
-      await initialise(isPublicRelease: false);
-
-      return logFileMap;
-    } catch (e) {
-      logFileMap["FILES"] = "Failed to read log files:\n$e";
-    }
-
-    return logFileMap;
-  }
-
-  /// [Internal Function]
-  /// Adds the file entity (defined by `entity`) to the `Map` of log files. Each
-  /// log file is added by its basename *without extensions*, and its contents
-  /// are written in one long string. Once a file has been read, it is deleted.
-  ///
-  /// This function handles missing files and unreadable files. It also handles
-  /// if a file fails to be delete.
-  static Future<void> _updateLogFileMapWithFile(
-      Map<String, String> logFileMap, FileSystemEntity entity) async {
-    final baseName = basenameWithoutExtension(entity.path);
-    final file = File(entity.path);
-    if (await file.exists()) {
-      try {
-        logFileMap[baseName] = await file.readAsString();
-      } catch (e) {
-        logFileMap[baseName] = "File was determined as 'existing', but could not be read\n$e";
-      }
-    } else {
-      logFileMap[baseName] = "Log file ceased to exist.";
-    }
-
-    // File has been read (or tried to), delete the file.
-    try {
-      file.deleteSync();
-    } catch (e) {
-      logFileMap["$baseName-DELETE"] = "Failed to delete log file\n$e";
-    }
   }
 
   static void navigateToLogsListView(final BuildContext context) {
